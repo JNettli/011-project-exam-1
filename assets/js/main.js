383854a28acadb2f512a5173d2d16d7f802ab313
Jonas Nettli <jonas.nettli@gmail.com>
2024-05-02 11:38:43 +0200

    Added main files and simple start to main website

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+const url = 'https://v2.api.noroff.dev/blog/posts/jnettli';
+
+getData();
+
+async function getData() {
+    try {
+        const res = await fetch(url);
+        if (!res.ok) {
+            throw new Error("Could not fetch resource!");
+        }
+    const data = await res.json();
+    console.log('Success: ', data.data);
+    console.log(data.data[0].title)
+    } catch (error) {
+        console.error('There was an error: ', error);
+    }
+}
+
+function sendPost() {
+    console.log("Hello! :)");
+}
+
+function displayPosts(data) {
+    const postTitle = document.createElement("div");
+    postTitle.classList.add("postTitle");
+    const postDiv = document.createElement("div");
+    postTitle.innerText = data.data.title;
+    postDiv.classList.add("postDiv");
+    const postContent = document.createElement("div");
+    postContent.innerText = data.data.body;
+    postContent.classList.add("postContent");
+    const postAuthor = document.createElement("div");
+    postAuthor.classList.add("postAuthor");
+    const postDate = document.createElement("div");
+    postDate.innerText = data.data.created;
+    postDate.classList.add("postDate");
+    const postImage = document.createElement("img");
+    postImage.src = `${data.data.media.url}`;
+    postImage.alt = `Beautiful post image!`;
+    postImage.classList.add("postImage");
+
+}
